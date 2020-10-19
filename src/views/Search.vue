5634bcd29f08a1016b36e647a488f1d4a8c66210
gkoum <koumoutsosgian@yahoo.gr>
2020-10-19 09:38:12 +0300

    inserted skeleton search and results pages into router

---
+++ src/views/Search.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{title}}
+    <img alt="Vue logo" src="../assets/logo.png" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Search",
+  data() {
+    return {
+      title: ""
+    };
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/views/SearchResults.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    Results
+  </div>
+</template>
+
+<script>
+
+export default {
+
+};
+</script>
