7ecf6707dd96edc6a411b831de1a1ded41c24cca
gkoum <koumoutsosgian@yahoo.gr>
2020-10-19 11:51:20 +0300

    added style and input to search page

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,24 +1,40 @@
 <template>
   <div>
+    <img alt="imdb logo" src="../assets/logo.png" />
     <h2>Search For Movies</h2>
     <div class="container">
-      <img alt="Vue logo" src="../assets/logo.png" />
-      <BaseButton
-        type="button"
-        @click="search"
-        class="button--primary button--inside"
-      >
-        <span>Find</span>
-      </BaseButton>
+      <form class="form">
+        <input
+          class="form__field"
+          placeholder="Movie Title"
+          @keydown.enter.exact.prevent
+          @keyup.enter.exact="search"
+          v-model="title"
+        />
+        <BaseButton
+          type="button"
+          @click="search"
+          class="button--primary button--inside"
+        >
+          <span>Find</span>
+        </BaseButton>
+      </form>
+      <p>created by Giannis Koumoutsos</p>
     </div>
   </div>
 </template>
 
 <script>
+import BaseButton from "@/components/_base-button.vue";
+
 export default {
   name: "Search",
+  components: {
+    BaseButton
+  },
   data() {
     return {
+      title: ""
     };
   },
   methods: {
@@ -60,6 +76,22 @@ body,
   }
   &--inside {
     margin-left: -16px;
+  }
+}
+
+
+//** form
+.form {
+  width: 500px;
+  &__field {
+    width: 70%;
+    background: #fff;
+    color: $input-text-color;
+    font: inherit;
+    box-shadow: 0 0 10px 2px #ffa000;
+    border: 0;
+    outline: 0;
+    padding: 22px 18px;
   }
 }
 </style>
