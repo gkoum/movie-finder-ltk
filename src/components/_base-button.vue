57a9b6f1b4ffad68d3db217cd28ecd5507965c74
gkoum <koumoutsosgian@yahoo.gr>
2020-10-19 11:45:56 +0300

    added base-button to search page

---
+++ src/components/_base-button.vue
@@ -0,0 +1,46 @@
+<template>
+  <button :class="$style.button" v-on="$listeners">
+    <slot />
+  </button>
+</template>
+
+<style lang="scss" module>
+@import 'src/design/index.scss';
+
+.button {
+  @extend %typography-small;
+
+  border-radius: 0px 25px 25px 0px;
+  display: inline-block;
+  font: inherit;
+  outline: 0;
+  transition: all 200ms ease-in;
+  padding: $size-button-padding;
+  color: $color-button-text;
+  cursor: pointer;
+  background: $color-button-bg;
+  border: none;
+
+  &--primary {
+    
+    color: $color-button-text;
+    box-shadow: 0 0 2px 2px #ffa000;
+    border-radius: 2px;
+    padding: 12px 36px;
+
+    &:hover {
+      background: darken($color-button-bg, 24%);
+    }
+
+    &:active {
+      background: #867455;
+      box-shadow: inset 0 0 10px 2px rgba(0, 0, 0, 0.2);
+    }
+  }
+
+  &:disabled {
+    cursor: not-allowed;
+    background: $color-button-disabled-bg;
+  }
+}
+</style>
--- src/views/Search.vue
+++ src/views/Search.vue
@@ -1,7 +1,16 @@
 <template>
   <div>
-    {{title}}
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <h2>Search For Movies</h2>
+    <div class="container">
+      <img alt="Vue logo" src="../assets/logo.png" />
+      <BaseButton
+        type="button"
+        @click="search"
+        class="button--primary button--inside"
+      >
+        <span>Find</span>
+      </BaseButton>
+    </div>
   </div>
 </template>
 
@@ -10,11 +19,47 @@ export default {
   name: "Search",
   data() {
     return {
-      title: ""
     };
   },
+  methods: {
+    search() {
+
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+@import 'src/design/index.scss';
+
+//** root
+:root {
+  background: $background;
+  color: $color-text;
+  font: 1rem "PT Sans", sans-serif;
+}
+
+html,
+body,
+.container {
+  height: 100%;
+}
+
+//** helper
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+
+//** button
+.button {
+  &--primary {
+    height: 60px;
+  }
+  &--inside {
+    margin-left: -16px;
+  }
+}
 </style>
